192f775e8d97eb8d86c9a053b088d1863fb2019c
Remi2222 <maryam.fajri02@gmail.com>
2026-04-21 15:52:59 +0100

    fix: frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://facturlybackend.up.railway.app/',
 });
 
 api.interceptors.request.use((config) => {
