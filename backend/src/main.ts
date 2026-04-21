038ddacb8fa83a0eede9fa047072333c6c30421e
Remi2222 <maryam.fajri02@gmail.com>
2026-04-21 22:29:46 +0100

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://facturly.up.railway.app',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   app.useGlobalFilters(new HttpExceptionFilter());
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port);
